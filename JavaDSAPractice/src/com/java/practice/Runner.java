561d513d610d18faf11c9f978f1f7f16bc9e03f4
tarunkumar81990 <tarunkumar81990@gmail.com>
2026-01-04 01:37:34 +0530

    Java common Programs

--- JavaDSAPractice/src/com/java/practice/Runner.java
+++ JavaDSAPractice/src/com/java/practice/Runner.java
@@ -1,16 +1,15 @@
 package com.java.practice;
 
-import java.util.Arrays;
-
 public class Runner {
 
 	public static void main(String[] args) throws Exception {
-		int[] input= {2,6,11,15,8,1};
-		String input2="abcdabghtycabcde";
-		int input5=123321;
-		String input6="LVIII";
-		String[] input7={"flower","flow","flight"};
-		LeetCodePrograms obj1= new LeetCodePrograms();
+		//int[] input= {2,6,11,15,8,1};
+		//String input2="abcdabghtycabcde";
+		//int input5=123321;
+		//String input6="LVIII";
+		//String[] input7={"flower","flow","flight"};
+		String path="\\src\\com\\java\\testdata\\javaCode.txt";
+		CommonPrograms obj1= new CommonPrograms();
 		//int[] result1=obj1.twoSumProgramBruteForce(input, 9);
 		//int[] result2=obj1.twoSumProgramOptimized(input, 9);
 		//int result3= obj1.longestSubstringLength(input2);
@@ -18,8 +17,12 @@ public class Runner {
 		//System.out.print(Arrays.toString(result2));
 		//boolean result5=obj1.checkPalindrome(input3);
 		//int result6=obj1.convertRomanToInteger(input4);
-		String result7=obj1.longestCommonPrefix(input7);
-		System.out.println(result7);
+		//String result7=obj1.longestCommonPrefix(input7);
+		//System.out.println(result7);
+		//boolean result8=obj1.checkValidParentheses(path);
+		//System.out.println(result8);
+		int[] nums = {0,0,1,1,1,2,2,3,3,4};
+		obj1.removeDuplicateSortedArray(nums);
 
 	}
 
